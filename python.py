fb77b341d9921614fbd94f9323ed638bdc9b590d
rlaehgml28 <153513489+rlaehgml28@users.noreply.github.com>
2023-12-13 11:38:17 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,87 @@
+def print_address() :
+...     pritn('경상북도')
+...     print('울릉군 울릉읍')
+...     print('독도리 산 1-92번지')
+... print_abbress()
+SyntaxError: invalid syntax
+>>> print('경상북도')
+경상북도
+>>> print('울릉군 울릉읍')
+울릉군 울릉읍
+>>> print('독도리 산 1-96번지')
+독도리 산 1-96번지
+>>> print_address()
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print_address()
+NameError: name 'print_address' is not defined
+def print_address(name):
+  print("서울 특별시 종로구 1번지")
+  print("파이썬 빌딩 7층")
+  print(name)
+print_address("홍길동")
+print_address("널널한 교수") 
+ KeyboardInterrupt
+def sort_num(n1,n2):
+    if n1 < n2:
+        return n1, n2
+    else:
+KeyboardInterrupt
+def sort_num(n1,n2):
+    if n1 < n2:
+        return n1, n2
+    else:
+        return n2,n1
+    print(sort_num(110.210))
+    print(sort_num(2100,80))
 
+    
+def calc(n1 , n2):
+    return n1 + n2, n1 - n2, n1 * n2, n1/ n2
+n1, n2 = 200,100
+SyntaxError: invalid syntax
+import turtle
+t = turtle.Turtle
+t.shape("turtle")
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    t.shape("turtle")
+  File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\lib\turtle.py", line 2774, in shape
+    return self.turtle.shapeIndex
+AttributeError: 'str' object has no attribute 'turtle'. Did you mean: 'title'?
+AttributeError: 'str' object has no attribute 'turtle'. Did you mean: 'title'?
+SyntaxError: invalid syntax
+def square(lengh):
+    for  i in range(4):
+        t.forward(length)
+        t.left(90)
+
+        
+def n_polygon(n, lengthh):
+    for i in range(n):
+        t.forward(length)
+        t.left(360//n)
+
+        
+def print_counter():
+    print('counter =', counter)
+    counter = 100
+    print_counter()
+    print('counter =', counter)
+
+    
+def square(n):
+    return n1,n1
+print('3의제곱은 : ' ,3,6,9)
+SyntaxError: invalid syntax
+def max2(m,n):
+    if range(2)
+    
+SyntaxError: incomplete input
+def max2(m,n):
+    if n1 < n2:
+        return n1,n2
+    else:
+        return n2,n1
+    def min2(m,n):
+        if
